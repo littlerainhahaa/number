f257898904998a3132ab8e606eaced8815097917
Yu-ting, Hsu <110767545+littlerainhahaa@users.noreply.github.com>
2022-11-15 16:26:06 +0800

    Add files via upload
    add count

--- number.py
+++ number.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1 #count = count + 1
     num = input('請輸入數字:')
     num = int(num)
     if num < r:
@@ -9,4 +11,6 @@ while True:
         print('比答案大')
     else:
         print('終於猜對了!')
+        print('你已經猜了', count, '次')
         break
+    print('你已經猜了', count, '次')
